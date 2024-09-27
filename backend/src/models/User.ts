460a28f533ed1ef8581b4db9bb0e199e2b2f32bc
piyusss11 <piyusss11@gmail.com>
2024-09-27 18:59:53 +0530

    validate email password and photo

--- backend/src/models/User.ts
+++ backend/src/models/User.ts
@@ -1,4 +1,5 @@
 import mongoose from "mongoose";
+import validator from "validator";
 
 const userSchema = new mongoose.Schema(
   {
@@ -28,11 +29,21 @@ const userSchema = new mongoose.Schema(
       required: true,
       unique: true,
       trim: true,
+      validate(value: string) {
+        if (!validator.isEmail(value)) {
+          throw new Error("email not valid: " + value);
+        }
+      },
     },
     password: {
       type: String,
       required: true,
       minLength: 8,
+      validate(value: string) {
+        if (!validator.isStrongPassword(value)) {
+          throw new Error("password not strong enough: " + value);
+        }
+      },
     },
     gender: {
       type: String,
@@ -42,6 +53,11 @@ const userSchema = new mongoose.Schema(
       type: String,
       default:
         "https://w7.pngwing.com/pngs/177/551/png-transparent-user-interface-design-computer-icons-default-stephen-salazar-graphy-user-interface-design-computer-wallpaper-sphere-thumbnail.png",
+      validate(value: string) {
+        if (!validator.isURL(value)) {
+          throw new Error("not a valid photo url: " + value);
+        }
+      },
     },
     about: {
       type: String,
