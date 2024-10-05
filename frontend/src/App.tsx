b88fff637d53c52879b2965e17b0c2ce6e98de22
piyusss11 <piyusss11@gmail.com>
2024-10-05 17:28:05 +0530

    edited nav

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,8 +3,8 @@ import Navbar from "./components/Navbar";
 
 function App() {
   return (
-    // <div className="h-screen bg-gradient-to-l from-[#2c2b30]  to-[#4f4f51]">
-     <div className="h-screen bg-[#2c2b30]"> 
+    <div className="h-screen bg-gradient-to-t from-[#2c2b30]  to-[#4f4f51]">
+     {/* <div className="h-screen bg-[#2c2b30]">  */}
 
       <Navbar />
     </div>
